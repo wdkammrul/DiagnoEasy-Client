46cab567555bb278c17f17ba4406002751148c6b
Md Kamrul Islam <kamruldca@gmail.com>
2023-11-26 13:50:26 +0600

    swaip add successfully

--- src/Pages/Home/PersonalizedSlider/PersonalizedSlider.jsx
+++ src/Pages/Home/PersonalizedSlider/PersonalizedSlider.jsx
@@ -1,29 +1,45 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 const PersonalizedSlider = () => {
     return (
-        <div>
-            <section className="bg-slate-800 p-8 mt-8">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-semibold text-gray-800 mb-4">Personalized Recommendations</h2>
-       
-                    <div className="swiper-container">
-                        <div className="swiper-wrapper">
-                   
-                            <div className="swiper-slide bg-gray-200 p-4 rounded shadow-md">
-                                <p className="text-gray-600">Health Tip 1</p>
-                            </div>
-                            
-                            <div className="swiper-slide bg-gray-200 p-4 rounded shadow-md">
-                                <p className="text-gray-600">Health Tip 2</p>
-                            </div>
-                     
-                        </div>
-
-                        <div className="swiper-pagination"></div>
-                    </div>
-                </div>
-            </section>
-
-        </div>
+        <section>
+            
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                // modules={[Pagination]}
+                className="mySwiper mb-24"
+            >
+                <SwiperSlide>
+                    <img src="https://i.ibb.co/0r58x4j/kamrul1.jpg" alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://i.ibb.co/0r58x4j/kamrul1.jpg" alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Pizzas</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://i.ibb.co/0r58x4j/kamrul1.jpg" alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://i.ibb.co/0r58x4j/kamrul1.jpg" alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Desserts</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://i.ibb.co/0r58x4j/kamrul1.jpg" alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+                </SwiperSlide>
+            </Swiper>
+        </section>
     );
 };
 
